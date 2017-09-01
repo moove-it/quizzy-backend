a1ebe7acd38d6b76aab8b8ffe7d828f9df580005
Guillermo Eijo <guillermo.eijo@fing.edu.uy>
2017-08-31 22:29:50 -0300

    Add matches_view and comment out parts of the match model

--- app/models/match.js
+++ app/models/match.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 
 var matchSchema = new Schema ({
+<<<<<<< 0dcb3135643b07e52e06ecd31f3b2f688100a6d9
 	url: {
     type: String,
     unique: true
@@ -32,6 +33,30 @@ var matchSchema = new Schema ({
 			image : String
 	},
 	result: [Number]
+=======
+    // id : { 
+    //     type: Number,
+    //     unique: true
+    // },
+    isRealTime: Boolean,
+    players: [String],
+    owner: String,
+    endingDate: Date,
+    game: {
+        // id : { type: Number, index: true },
+        name: String,
+        description: String,
+        rating: Number,
+        timesPlayed: Number,
+        creator: String,
+        questions: [String],
+        tags: [String],
+        // ranking : [(String, Number)],
+        creationDate: Date,
+        image: String
+    },
+    result: [Number]
+>>>>>>> Add matches_view and comment out parts of the match model
 })
 
 class MatchClass {}
