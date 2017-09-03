972a26666b2f8c74efd4cce7f731c3b24b29c482
Guillermo Eijo <guillermoeijo@gmail.com>
2017-09-03 18:31:22 -0300

    Add match factory and  basic test functionality

--- spec/factories/factory.js
+++ spec/factories/factory.js
@@ -1,5 +1,6 @@
 import factory from 'factory-girl';
 import User from '../../app/models/user';
+import Match from '../../app/models/match';
 import faker from 'faker';
 
 factory.define('user', User,
@@ -8,5 +9,54 @@ factory.define('user', User,
     password: () => faker.internet.password()
   }
 );
+
+factory.define('match', Match, {
+    isRealTime: () => faker.random.boolean(),
+    players: ['player1', 'player2', 'player3'],
+    owner: () => faker.fake('{{name.firstName}} {{name.lastName}}'),
+    endingDate: () => faker.date.future(),
+    game: {
+        // id: { type: () => faker.random.number },
+        name: () => faker.fake('{{name.firstName}} {{name.lastName}}'),
+        description: () => faker.lorem.sentence(),
+        rating: () => faker.random.number(),
+        timesPlayed: () => faker.random.number(),
+        creator: () => faker.fake('{{name.firstName}} {{name.lastName}}'),
+        questions: [
+            {
+                // id: { type: () => faker.random.number() },
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            },
+            {
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            },
+            {
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            },
+            {
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            }],
+        tags: ['tag1', 'tag2', 'tag3'],
+        ranking: [{
+            user: () => faker.internet.userName(),
+            points: () => faker.random.number()
+        }],
+        endingDate: () => faker.date.past(),
+        image: () => faker.image.imageUrl()
+    },
+    result: [1, 2, 3]
+});
 
 export default factory;
